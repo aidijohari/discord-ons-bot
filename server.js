f2145ad0bca38a34b8e7593f715ccb1acdbd5217
Aidi Johari <59901507+aidijohari@users.noreply.github.com>
2025-07-12 15:32:10 +0800

    v3

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const app = express();
-// const fetch = require('node-fetch');
+// const express = require("express");
+// const app = express();
+// // const fetch = require('node-fetch');
 
-const PORT = process.env.PORT || 3001
+// const PORT = process.env.PORT || 3001
 
-app.get("/", (req, res) => {
-    res.send("🤖 Bot is online");
-});
+// app.get("/", (req, res) => {
+//     res.send("🤖 Bot is online");
+// });
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, "0.0.0.0", () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
-app.get("/health", (req, res) => {
-  res.status(200).send("OK");
-});
+// app.get("/health", (req, res) => {
+//   res.status(200).send("OK");
+// });
