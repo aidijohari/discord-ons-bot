093aa79c2be8b5293f2f351d9eb9b75a1b5bbefc
Aidi Johari <59901507+aidijohari@users.noreply.github.com>
2025-07-12 11:29:07 +0800

    changed port due to old one not dying after redeploy

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const app = express();
 
 // const fetch = require('node-fetch');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 // app.get("/", (req, res) => {
 //     res.send("Bot is online");
