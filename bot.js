18695f2ad80946897719d9951ce06a82130990c5
Aidi Johari <59901507+aidijohari@users.noreply.github.com>
2025-07-12 11:39:26 +0800

    added health check /health

--- bot.js
+++ bot.js
@@ -30,6 +30,11 @@ app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
